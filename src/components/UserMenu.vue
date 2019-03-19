11cbc7d2eb5af13970415f68c3c51a70ab8f2ed3
mzb0312 <mzb0312@163.com>
2019-03-19 17:07:23 +0800

    1

---
+++ src/components/UserMenu.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="user-wrapper">
+        <header-notice class="action"></header-notice>
+        <a-dropdown>
+            <span class="action ant-dropdown-link user-dropdown-menu">
+                <a-avatar icon="user" size="small"/>
+            <span> 一杯闲</span>
+            </span>
+                <a-menu slot="overlay" class="user-dropdown-menu-wrapper" placement="bottomRight">
+                <a-menu-item key="0">
+                    <router-link :to="{ name: 'center' }">
+                        <a-icon type="user"/>
+                        <span>个人中心</span>
+                    </router-link>
+                </a-menu-item>
+                <a-menu-item key="1">
+                    <router-link :to="{ name: 'settings' }">
+                        <a-icon type="setting"/>
+                        <span>账户设置</span>
+                    </router-link>
+                </a-menu-item>
+                <a-menu-item key="2" disabled>
+                    <a-icon type="setting"/>
+                    <span>测试</span>
+                </a-menu-item>
+                <a-menu-divider/>
+                <a-menu-item key="3">
+                    <a href="javascript:;" @click="handleLogout">
+                        <a-icon type="logout"/>
+                        <span>退出登录</span>
+                    </a>
+                </a-menu-item>
+            </a-menu>
+        </a-dropdown>
+    </div>
+</template>
+
+<script>
+    import HeaderNotice from './HeaderNotice'
+    export default {
+        name: 'UserMenu',
+        components: {
+            HeaderNotice
+        },
+        methods: {
+            handleLogout() {
+                this.$confirm({
+                    title: '提示',
+                    content: '真的要注销登录吗 ?',
+                    onOk() {
+                        window.location.reload()
+                    },
+                    onCancel() {
+                    }
+                })
+            }
+        }
+    }
+</script>
+<style scoped>
+    .user-wrapper {
+        float: right;
+        height: 100%;
+    }
+
+    .user-wrapper .action:hover {
+        background: rgba(0, 0, 0, .025);
+    }
+
+    .user-wrapper .action {
+        cursor: pointer;
+        padding: 0 12px;
+        display: inline-block;
+        -webkit-transition: all .3s;
+        transition: all .3s;
+        height: 100%;
+        color: rgba(0, 0, 0, .65);
+    }
+    .user-dropdown-menu-wrapper.ant-dropdown-menu .ant-dropdown-menu-item {
+        width: 160px;
+    }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,128 @@
+<template>
+    <a-layout class="layout">
+        <a-layout-sider
+                :trigger="null"
+                collapsible
+                v-model="collapsed"
+                width="256px"
+        >
+            <div class="logo">
+                <LogoSvg alt="logo"/>
+                <h1>Ant Template</h1>
+            </div>
+            <a-menu theme="dark" mode="inline" :defaultSelectedKeys="['1']" style="padding: 16px 0px;">
+                <a-menu-item key="1">
+                    <a-icon type="user"/>
+                    <span>nav 1</span>
+                </a-menu-item>
+                <a-sub-menu
+                        key="sub1"
+                >
+                    <span slot="title"><a-icon type="user"/><span>User</span></span>
+                    <a-menu-item key="3">Tom</a-menu-item>
+                    <a-menu-item key="4">Bill</a-menu-item>
+                    <a-menu-item key="5">Alex</a-menu-item>
+                </a-sub-menu>
+                <a-menu-item key="2">
+                    <a-icon type="video-camera"/>
+                    <span>nav 2</span>
+                </a-menu-item>
+                <a-menu-item key="3">
+                    <a-icon type="upload"/>
+                    <span>nav 3</span>
+                </a-menu-item>
+            </a-menu>
+        </a-layout-sider>
+        <a-layout>
+            <a-layout-header style="background: #fff; padding: 0">
+                <div class="header">
+                    <a-icon
+                            class="trigger"
+                            :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+                            @click="()=> collapsed = !collapsed"
+                    />
+                    <user-menu></user-menu>
+                </div>
+            </a-layout-header>
+            <a-layout-content :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }">
+                Content
+            </a-layout-content>
+            <a-layout-footer style="textAlign: center">
+                Ant Design Admin ©2019 Created by mzb
+            </a-layout-footer>
+        </a-layout>
+    </a-layout>
+</template>
+<script>
+    import LogoSvg from '@/assets/logo.svg?inline'
+    import UserMenu from '../components/UserMenu'
+
+    export default {
+        components: {
+            LogoSvg,
+            UserMenu
+        },
+        data() {
+            return {
+                collapsed: false,
+            }
+        },
+    }
+</script>
+<style>
+    .layout {
+        height: 100%;
+        text-align: left;
+    }
+
+    .layout .trigger {
+        font-size: 18px;
+        line-height: 64px;
+        padding: 0 24px;
+        cursor: pointer;
+        transition: color .3s;
+    }
+
+    .layout .trigger:hover {
+        color: #1890ff;
+    }
+
+    .layout .logo {
+        height: 64px;
+        position: relative;
+        line-height: 64px;
+        padding-left: 24px;
+        -webkit-transition: all .3s;
+        transition: all .3s;
+        background: #002140;
+        overflow: hidden;
+    }
+
+    .layout .logo h1 {
+        color: #fff;
+        font-size: 20px;
+        margin: 0 0 0 12px;
+        font-family: Chinese Quote, -apple-system, BlinkMacSystemFont, Segoe UI, PingFang SC, Hiragino Sans GB, Microsoft YaHei, Helvetica Neue, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol;
+        font-weight: 600;
+
+    }
+
+    .layout .logo h1, .layout .logo img, .logo svg {
+        display: inline-block;
+        vertical-align: middle;
+    }
+
+    .layout .logo img, .layout .logo svg {
+        height: 32px;
+        width: 32px;
+    }
+
+    .layout.ant-layout .header {
+        height: 64px;
+        padding: 0 12px 0 0;
+        background: #fff;
+        -webkit-box-shadow: 0 1px 4px rgba(0,21,41,.08);
+        box-shadow: 0 1px 4px rgba(0,21,41,.08);
+        position: relative;
+    }
+</style>
